58db16e179a53235b5204cac2b08b358ce0cc7c9
Mateo Ortiz <60745536+MateoOrtiz001@users.noreply.github.com>
2020-12-02 00:12:13 -0500

    Add files via upload

---
+++ forjaz/mapaInteractivoCel.js
@@ -0,0 +1,46 @@
+/*** CÓDIGO DE JS PARA EL MAPA INTERACTIVO EN VENTANA MENOR A 600px***/
+
+/*** Funciones de cambio de contenido ***/
+/** Cambia al texto e imagen**/
+function cambiarTexto1() {
+   document.getElementById("textoCel").innerHTML = "1. El Trono: Lugar donde el consejo de los tres martillos trabajan todo el día sobre las decisiones tomadas en el reino de Khaz Modan.";/**Cambia el texto con Id "textoCel" **/
+   document.getElementById("imgCel").src = "imagenes/imgCel01.jpg";/**Cambia la imagen con el Id "imgCel"**/
+}
+
+function cambiarTexto2() {
+   document.getElementById("textoCel").innerHTML = "2. La Gran Forja: La Gran forja es una gran muestra del ingenio de los enanos. Lograron tomar tres cascadas de lava ardiente para crear un sistema en donde aprovechan esta lava para la forja de sus más grandes armas y armaduras.";
+    document.getElementById("imgCel").src = "imagenes/imgCel02.jpg";
+}
+function cambiarTexto3() {
+   document.getElementById("textoCel").innerHTML = "3. La Libreria: Es, después de la torre de Kharazan en el Paso de la Muerte, la fuente de mayor concimiento de las Tierras del Este. Los grandes descubrimientos arqueológicos se encuentran en exposición aquí. ";
+    document.getElementById("imgCel").src = "imagenes/imgCel03.jpg";
+}
+function cambiarTexto4() {
+   document.getElementById("textoCel").innerHTML = "4. La Plaza: La plaza es el lugar más concurrido de la ciudad por varias razones: las arcas de la ciudad, la casa de subastas y la entrada de la ciudad. Es por esta razón que es el eje de la economía en la capital.";
+    document.getElementById("imgCel").src = "imagenes/imgCel04.jpg";
+}
+function cambiarTexto5() {
+   document.getElementById("textoCel").innerHTML = "5. La Sala Militar: Aquí es donde se forman a los mayores guerreros, paladines y cazadores de la región. Aquí destacan las grandes tiendas de armamento y la fábrica de armas de guerra.";
+    document.getElementById("imgCel").src = "imagenes/imgCel05.jpg";
+}
+function cambiarTexto6() {
+   document.getElementById("textoCel").innerHTML = "6. La Sala Arcana: Lugar donde son formados los más grandes sacerdotes, magos, chamanes y algunos paladines. Aquí los aprendices son imbuídos con el poder de la mágia, principalmente la arcana.";
+    document.getElementById("imgCel").src = "imagenes/imgCel06.jpg";
+}
+function cambiarTexto7() {
+   document.getElementById("textoCel").innerHTML = "7. La Caverna Abandonada: Existen decenas de mitos sobre este lugar, lo cierto es que aquí, desde hace varios eones, los enanos han perfeccionado el arte de la pesca en el gran estanque. Cierto sí es, que no se conoce muy bien las verdaderas razones por las que este lugar no fue tallado como el resto de la ciudad.";
+    document.getElementById("imgCel").src = "imagenes/imgCel07.jpg";
+}
+function cambiarTexto8() {
+   document.getElementById("textoCel").innerHTML = "8. La Sala Industrial / Nueva Ciudad Manitas: Sitio que ha acogido a los gnomos de Gnomeregan, allí, los hermanos más cercanos de los enanos están todo el tiempo inventando todo tipo de artilugios en nombre de la ciencia.";
+    document.getElementById("imgCel").src = "imagenes/imgCel08.jpg";
+}
+function cambiarTexto9() {
+   document.getElementById("textoCel").innerHTML = "9. El Tranvía: Esta es la estación del tranvia, una de las mayores obras de ingenieria de los Reinos del Este. Este tranvía conduce hacia Ventormenta, la capital de los humanos, conectando con el Barrio de los Enanos en dicha capital.";
+    document.getElementById("imgCel").src = "imagenes/imgCel09.jpg";
+}
+function cambiarTexto10() {
+   document.getElementById("textoCel").innerHTML = "10. La Vieja Forjaz: Lugar ubicado en lo más profundo de la montaña, su ingreso desde el trono lo hace un lugar ideal en donde los habitantes de la ciudad pueden protegerse en el caso de un hipotético ataque enemigo.";
+    document.getElementById("imgCel").src = "imagenes/imgCel10.jpg";
+}
+
