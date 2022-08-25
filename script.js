eaa37012b73e14b37b5c5d7f23f78a376539bfa8
Mateo Ortiz <60745536+MateoOrtiz001@users.noreply.github.com>
2022-08-24 19:41:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function textoEnanoIn(){
+    document.getElementById("textEnano").style.display= "block";
+}
+
+function textoEnanoOut(){
+    document.getElementById("textEnano").style.display= "none";
+}
