a88aac29502c5221da8b3958c1d7e1a5ea307ea5
Mateo Ortiz <60745536+MateoOrtiz001@users.noreply.github.com>
2022-11-09 14:48:35 -0500

    Add files via upload

--- forjaz/scripts.js
+++ forjaz/scripts.js
@@ -14,3 +14,92 @@ function oscurecer(){
         oscurecido = false;
     }
 }
+
+var cGaleriaNum = 1;
+function adelanteGaleria(){
+    var imagenGaleria = document.getElementById('cGImg');
+    if(cGaleriaNum == 1){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/02.jpg";
+        cGaleriaNum = 2;
+    }else if(cGaleriaNum == 2){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/03.jpg";
+        cGaleriaNum = 3;
+    }else if(cGaleriaNum == 3){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/04.jpg";
+        cGaleriaNum = 4;
+    }else if(cGaleriaNum == 4){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/05.jpg";
+        cGaleriaNum = 5;
+    }else if(cGaleriaNum == 5){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/06.jpg";
+        cGaleriaNum = 6;
+    }else if(cGaleriaNum == 6){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/07.jpg";
+        cGaleriaNum = 7;
+    }else if(cGaleriaNum == 7){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/08.jpg";
+        cGaleriaNum = 8;
+    }else if(cGaleriaNum == 8){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/09.jpg";
+        cGaleriaNum = 9;
+    }else if(cGaleriaNum == 9){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/10.jpg";
+        cGaleriaNum = 10;
+    }else if(cGaleriaNum == 10){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/11.jpg";
+        cGaleriaNum = 11;
+    }else if(cGaleriaNum == 11){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/12.jpg";
+        cGaleriaNum = 12;
+    }else if(cGaleriaNum == 12){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/13.jpg";
+        cGaleriaNum = 13;
+    }else if(cGaleriaNum == 13){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/01.jpg";
+        cGaleriaNum = 1;
+    }
+}
+
+function atrasGaleria(){
+    var imagenGaleria = document.getElementById('cGImg');
+    if(cGaleriaNum == 1){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/13.jpg";
+        cGaleriaNum = 13;
+    }else if(cGaleriaNum == 2){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/01.jpg";
+        cGaleriaNum = 1;
+    }else if(cGaleriaNum == 3){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/02.jpg";
+        cGaleriaNum = 2;
+    }else if(cGaleriaNum == 4){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/03.jpg";
+        cGaleriaNum = 3;
+    }else if(cGaleriaNum == 5){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/04.jpg";
+        cGaleriaNum = 4;
+    }else if(cGaleriaNum == 6){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/05.jpg";
+        cGaleriaNum = 5;
+    }else if(cGaleriaNum == 7){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/06.jpg";
+        cGaleriaNum = 6;
+    }else if(cGaleriaNum == 8){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/07.jpg";
+        cGaleriaNum = 7;
+    }else if(cGaleriaNum == 9){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/08.jpg";
+        cGaleriaNum = 8;
+    }else if(cGaleriaNum == 10){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/09.jpg";
+        cGaleriaNum = 9;
+    }else if(cGaleriaNum == 11){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/10.jpg";
+        cGaleriaNum = 10;
+    }else if(cGaleriaNum == 12){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/11.jpg";
+        cGaleriaNum = 11;
+    }else if(cGaleriaNum == 13){
+        imagenGaleria.src = "imagenes/cGaleriaImgs/12.jpg";
+        cGaleriaNum = 12;
+    }
+}
